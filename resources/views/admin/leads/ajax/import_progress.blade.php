ae951704037fd3170a03d4e3af599536ed6f175d
vishalnetbiz <vishal.kumar@netbiz.in>
2024-06-25 17:31:00 +0530

    new code

--- resources/views/admin/leads/ajax/import_progress.blade.php
+++
@@ -1,6 +0,0 @@
-@include('import.process-form', [
-    'headingTitle' => __('app.importExcel') . ' ' . __('app.menu.lead'),
-    'processRoute' => route('leads.import.process'),
-    'backRoute' => route('leads.index'),
-    'backButtonText' => __('app.backToLead'),
-])
--- resources/views/components/app-title.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- PAGE TITLE START -->
-<div {{ $attributes->merge(['class' => 'page-title']) }}>
-    <div class="page-heading">
-        <h2 class="mb-0 pr-3 text-dark f-18 font-weight-bold d-flex align-items-center">
-            <span class="d-inline-block text-truncate mw-300">{{ $pageTitle }}</span>
-        </h2>
-    </div>
-</div>
-<!-- PAGE TITLE END -->
